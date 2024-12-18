e92de408ff75f3db45191b2c8d1d9473d5a8e52d
zahra <zahra@LAPTOP-G3GO6AKN>
2024-12-18 23:48:20 +0300

    Add project files.

---
+++ KuaforSalonu/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace KuaforSalonu.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
