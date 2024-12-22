235a7c5e178684c8ac4a6092af50949ef13e983f
manar-alsayedali <133851112+manar-alsayedali@users.noreply.github.com>
2024-12-22 22:38:30 +0300

    Add files via upload

--- KuaforSalonu/Controllers/SalonController.cs
+++ KuaforSalonu/Controllers/SalonController.cs
@@ -1,6 +1,19 @@
-﻿namespace KuaforSalonu.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KuaforSalonu.Controllers
 {
-    public class SalonController
+    public class SalonController : Controller
     {
+        private readonly ILogger<HomeController> _logger;
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
     }
 }
