355c3b34bb8e71fb159ae2a4c378c21d1988eaa8
Lubana <133877642+Lubana54@users.noreply.github.com>
2024-12-29 09:45:38 -0800

    Gece olsa bile çalışmaya devam ;)

---
+++ KuaforSalonu/Controllers/LoginController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using Kuafor_Salonu.Models;
+using Kuafor_Salonu.Data;
+
+namespace Kuafor_Salonu.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AccountController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string username, string password)
+        {
+            var user = _context.Users
+                .FirstOrDefault(u => u.Username == username && u.Password == password);
+
+            if (user != null)
+            {
+                // Oturum açma işlemi (örnek: bir oturum çerezi oluştur)
+                HttpContext.Session.SetString("UserId", user.Id.ToString());
+                HttpContext.Session.SetString("Role", user.Role.ToString());
+
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewBag.Error = "Kullanıcı adı veya şifre hatalı.";
+                return View();
+            }
+        }
+
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Clear();
+            return RedirectToAction("Login", "Account");
+        }
+    }
+}
