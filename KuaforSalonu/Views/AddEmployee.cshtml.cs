c0fe459c5dd75dab02e3965bfd8b474780a0efb5
manar-alsayedali <133851112+manar-alsayedali@users.noreply.github.com>
2024-12-30 23:27:59 +0300

    Add files via upload

---
+++ KuaforSalonu/Views/AddEmployee.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Odev.Views.Admin
+{
+    public class AddEmployeeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ KuaforSalonu/Views/AddService.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Odev.Views.Admin
+{
+    public class AddServiceModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
